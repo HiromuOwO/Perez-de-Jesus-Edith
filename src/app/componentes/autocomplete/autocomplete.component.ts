3dffef65425bf17fb2958e59e7ca105e7df718d9
HiromuOwO <edithperezdejesus00@gmail.com>
2024-07-17 08:31:11 -0600

    first commit

---
+++ src/app/componentes/autocomplete/autocomplete.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-autocomplete',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.css'
+})
+export class AutocompleteComponent {
+
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+
+}
